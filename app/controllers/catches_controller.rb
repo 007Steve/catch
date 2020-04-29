5f0706f8bb5e6c6edef427935e592fb5df78973b
007steve <stephen.plummer1.sp@gmail.com>
2020-04-29 00:57:54 -0400

    added new erb and route

--- app/controllers/catches_controller.rb
+++ app/controllers/catches_controller.rb
@@ -1,11 +1,27 @@
 class CatchesController < ApplicationController
 #CREATE
     #new
-
+get "/catches/new" do
+        
+    erb :'/catches/new'
+    end
+    #create
+    post '/catches' do
+        new_catch = Catch.new(params)
+        #*
+        if new_catch.save
+  
+            redirect "/catches"
+        else
+             # @error = "Please fill in all information."
+              redirect "/catches/new"
+        end
+        
+      end
 #READ
     #index
 get "/catches" do
-    @catches = Catch.all
+    @catches = Catch.all.reverse
     erb :'catches/index'
 end
 
