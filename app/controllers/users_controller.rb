8960277c7aac4b83794a7c818ecbcd4dc16996bb
007steve <stephen.plummer1.sp@gmail.com>
2020-04-29 03:10:52 -0400

    added signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
 get "/signup" do
     erb :'users/signup'
 end
+post "/signup" do
+    user = User.new(params)
+    if user.save
+     session[:user_id] = user.id
+          redirect '/catches'
+    else
+        erb :'users/signup'
+    end
+end
 #LOGIN
 end
