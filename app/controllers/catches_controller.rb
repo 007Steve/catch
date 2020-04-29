0e95fd8afd1d18ad6707af31486dafb277881cb6
007steve <stephen.plummer1.sp@gmail.com>
2020-04-28 23:28:06 -0400

    dded index erb and route

--- app/controllers/catches_controller.rb
+++ app/controllers/catches_controller.rb
@@ -1,4 +1,20 @@
 class CatchesController < ApplicationController
+#CREATE
+    #new
 
+#READ
+    #index
+get "/catches" do
+    erb: index
+end
+
+    #show
+
+#UPDATE
+    #edit
+
+
+#DESTROY
+    #delete
 
 end
